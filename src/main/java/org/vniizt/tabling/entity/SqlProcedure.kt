20c6f217e822ee2853444614d6257f027ac2fc9e
Александр Ильин <ilin.aleksandr@vniizht.ru>
2023-06-22 00:22:05 +0300

    Fuck this

--- src/main/java/org/vniizt/tabling/entity/SqlProcedure.kt
+++ src/main/java/org/vniizt/tabling/entity/SqlProcedure.kt
@@ -10,7 +10,7 @@ class SqlProcedure(private var procedureText: String){
             .erase(bracketedComment)
             .trim()
             .replace(whitespace, " ")
-            .replace(semicolonSeparator, ";")
+            .replace(semicolonSeparatorQuotesIgnored, ";")
     }
 
     // Collecting quoted strings and replacing them with their own indices in the text
@@ -24,8 +24,6 @@ class SqlProcedure(private var procedureText: String){
         }
     }
 
-    val expressions = CompositeExpression(procedureText).expressions
-
     private companion object Regexes {
 
         private const val ignoreQuotesPattern = "(?=(?:[^']*'[^']*')*[^']*\$)"
@@ -35,7 +33,7 @@ class SqlProcedure(private var procedureText: String){
         val whitespace = Regex("\\s+")
         val simpleComment = Regex("--.*")
         val bracketedComment = Regex("/\\*.*\\*/")
-        val semicolonSeparator = Regex("\\s*;\\s*$ignoreQuotesPattern")
+        val semicolonSeparatorQuotesIgnored = Regex("\\s*;\\s*(?=(?:[^']*'[^']*')*[^']*\$)")
         val assignmentOperator = Regex("\\s?(=|:=)\\s?")
 
         val BEGINBody   = Regex("(?i)(?<=BEGIN\\s)[\\s\\S]*?(?=END(?: [^;]*|);\$)")
@@ -43,35 +41,10 @@ class SqlProcedure(private var procedureText: String){
         val quotedText = Regex("'(?:''|[^'])*'")
         val unescapedQuote = Regex("'(?!')")
 
-        val tableName = Regex("(?i)[a-z_][a-z0-9_]*?\\.[a-z_][a-z0-9_]*?")
-
-        val expression = Regex("$loopBlockPattern|$basicRowPattern")
-
-        val loopExpressionEnd = Regex("(?i)(?<=;| )END LOOP(?=;\$|\$)")
-        val loopExpressionCondition = Regex("(?i)(?i)(?<=^)[^;]*?(?= LOOP )")
+        val name = Regex("(?i)[a-z_][a-z0-9_]*")
+        val entityName = Regex("$name\\.$name")
     }
 
-    interface Expression
-
-    open inner class CompositeExpression(text: String): Expression{
-        val expressions: List<Expression> = text
-            .find(BEGINBody)?.value
-            ?.findAll(expression)?.toList()
-            ?.map { createExpression(it.value) }
-            ?: emptyList()
-    }
-
-    inner class BasicRowExpression(val text: String): Expression
-
-    inner class LoopBlockExpression(text: String): CompositeExpression(text){
-        val condition: String = text.find(loopExpressionCondition)?.value ?: ""
-    }
-
-    private fun createExpression(expressionText: String) =
-        if(expressionText.contains(loopExpressionEnd))
-            LoopBlockExpression(expressionText)
-        else
-            BasicRowExpression(expressionText)
 
     private fun String.erase(value: String) = replace(value, "")
     private fun String.erase(regex: Regex) = replace(regex, "")
--- src/main/java/org/vniizt/tabling/service/RelatedTablesResolver.kt
+++ src/main/java/org/vniizt/tabling/service/RelatedTablesResolver.kt
@@ -2,48 +2,56 @@ package org.vniizt.tabling.service
 
 import org.springframework.stereotype.Service
 import org.vniizt.tabling.entity.RelatedTables
-import org.vniizt.tabling.entity.SqlProcedure
-import org.vniizt.tabling.util.Regexes
 
 @Service
 class RelatedTablesResolver {
 
     fun findRelatedTables(procedureText: String) = HashSet<RelatedTables>().apply {
-        BEGINBody.find(
-            // Preparing for analysis
-            procedureText
-                .erase(simpleComment)
-                .erase("\n")
-                .erase(bracketedComment)
-                .replace(whitespace, " ")
-                .replace(semicolonSeparator, ";")
-                .trim()
-        )
+
+
+
+        procedureText
+            .prepareForAnalysis()
+            .split(semicolonSeparatorQuotesIgnored)
+            .forEach {
+//                this.add(it)
+            }
     }
 
-    private fun String.findUPDATEExpression() {
+    private fun String.findRelatedTables(): RelatedTables{
 
+    }
+
+    // Removes some unnecessary content, such as comments, space sequences, etc.
+    private fun String.prepareForAnalysis() = this
+        .erase(simpleComment)
+        .erase("\n")
+        .erase(bracketedComment)
+        .trim()
+        .replace(whitespace, " ")
+        .find(BEGINBody)?.value ?: ""
+
+    private companion object Regexes {
+        val whitespace = Regex("\\s+")
+        val simpleComment = Regex("--.*")
+        val bracketedComment = Regex("/\\*.*\\*/")
+        val semicolonSeparatorQuotesIgnored = Regex("\\s*;\\s*(?=(?:[^']*'[^']*')*[^']*\$)")
+        val assignmentOperator = Regex("\\s?(=|:=)\\s?")
+
+        val BEGINBody   = Regex("(?i)(?<=BEGIN\\s)[\\s\\S]*?(?=END(?: [^;]*|);\$)")
+
+        val quotedText = Regex("'(?:''|[^'])*'")
+
+        val name = Regex("\\D\\w*")
+        val entityName = Regex("$name\\.$name")
+
+        val targetTable = Regex("(?i)(?<=(?:^|\\W)(?:UPDATE|INSERT INTO) )\\D\\w*\\.\\D\\w*")
     }
 
     private fun String.erase(value: String) = replace(value, "")
     private fun String.erase(regex: Regex) = replace(regex, "")
+    private fun String.find(regex: Regex) = regex.find(this)
+    private fun String.findAll(regex: Regex) = regex.findAll(this)
 
-    private companion object Regexes {
-
-        val whitespace = Regex("\\s+")
-        val simpleComment = Regex("--.*")
-        val bracketedComment = Regex("/\\*.*\\*/")
-        val semicolonSeparator = Regex("\\s*;\\s*$ignoreQuotesPattern")
-        val assignmentOperator = Regex("\\s?(=|:=)\\s?")
-
-        val BEGINBody   = Regex("(?<=BEGIN\\s)[\\s\\S]*(?=\\sEND;$)")
-
-        val blockStartOperators = Regex("(?i)")
-        val blockEndOperators = Regex("(?i)")
-
-        val tableName = Regex("[a-z_][a-z0-9_]*?\\.[a-z_][a-z0-9_]*?")
-
-        private val ignoreQuotesPattern
-            get() = "(?=(?:[^']*'[^']*')*[^']*\$)"
-    }
+    private inner class
 }
